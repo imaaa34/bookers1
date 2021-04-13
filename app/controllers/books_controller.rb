5622f2524be5c0dba78fe74a8ad2c0df1daee00c
imaaa34 <oct.imd34@gmail.com>
2021-04-13 23:48:26 +0000

    [Add] updateにflashメッセージ追加、viewページの記述先変更

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -22,8 +22,10 @@ class BooksController < ApplicationController
 
   def update
     book = Book.find(params[:id])
-    book.update(book_params)
-    redirect_to book_path(book.id)
+    if book.update(book_params)
+      flash[:notice] = 'Book was successfully updated.'
+      redirect_to book_path(book.id)
+    end
   end
 
   def destroy
